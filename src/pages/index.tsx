7031f38587d2e316dce6462df3ec0f7984ddcd4d
Ajaxdw <ajaxdwdev@gmail.com>
2022-07-13 15:34:57 -0400

    basic ui structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,15 @@ import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
-    <div className="text-2xl text-red-200">Hello World!</div>
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-2xl text-center">Which Pokemon is Rounder?</div>
+      <div className="p-2"/>
+      <div className="border rounded p-8 flex justify-between max-w-2xl">
+        <div className="w-16 h-16 bg-red-200" />
+        <div className="p-8">Vs</div>
+        <div className="w-16 h-16 bg-red-200" />
+      </div>
+    </div>
   );
 };
 
